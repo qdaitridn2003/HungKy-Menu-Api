461cc8ef0f348f73939dcf1b5f244e841f7b7d91
qdaitridn2003 <qdaitridn2003@gmail.com>
2024-04-15 22:04:40 +0700

    Seven Commit

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' });
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
 
